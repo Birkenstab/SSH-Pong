ca4a9e72298ba2c8aa2e45a403b90414bb6f6d50
Moritz Beck (Birkenstab) <git@birkenstab.de>
2018-09-30 13:41:02 +0200

    Build script: Update latest tag

--- build.sh
+++ build.sh
@@ -7,3 +7,6 @@ PONG_VERSION=$(node -e "console.log(JSON.parse(require('fs').readFileSync('packa
 echo "Building version ${PONG_VERSION}"
 
 docker build -t birkenstab/ssh-pong:${PONG_VERSION} .
+
+# Update latest tag
+docker tag birkenstab/ssh-pong:1.0.0 birkenstab/ssh-pong
